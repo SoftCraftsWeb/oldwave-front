578dfc5a4d15ab7642c093babbb4c596cc7512b1
Valeria Granada Rodas <alejandro.castrillonc@udea.edu.co>
2022-07-25 22:36:49 -0500

    feat: add carrousel

--- src/presentation/pages/HomePage.jsx
+++ src/presentation/pages/HomePage.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Slider from 'presentation/components/atoms/Slider';
+import config from 'domain/config';
 
 export default function HomePage() {
-  return <div className='flex flex-col h-full w-full'>HOLIII</div>;
+  return (
+    <div className='flex flex-col h-full w-full'>
+      <div>
+        <Slider slides={config.banners} />
+      </div>
+    </div>
+  );
 }
